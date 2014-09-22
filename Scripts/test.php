d814aeb868e184efad886fb18d5acd8640d3e417
cert <dan700and2@gmail.com>
2014-09-21 22:45:35 -0700

    test commit

--- Scripts/test.php
+++ Scripts/test.php
@@ -4,6 +4,7 @@ include_once('/Users/constants.php');
 include_once(HOME_PATH.'WebbotsSpidersScreenScraper_Libraries_REV2_0/sweetfunctions.php');
 
 echo "HI"."\n";
+echo "Does this work";
 //send_email('test', 'test');
 
 ?>
