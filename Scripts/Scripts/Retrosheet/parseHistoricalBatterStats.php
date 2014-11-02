1afc2ffc567536b7fd2075571f8630a155f80469
cert <dan700and2@gmail.com>
2014-11-02 13:41:00 -0800

    Retrosheet Batter Pull: Add Handling Of Missing Splits
    Summary:
    Added function to handle splits with low sample sizes and
    accordingly add defaults. Fixed bug where career stats would only populate if a player
    had played that season by reversing the order of the foreach loops. Added number formatting
    of percentages to make more reading results in the forecoming JSON.
    
    Test Plan:
    Ran locally - output:
    {F380}
    
    Reviewers: smas
    
    Reviewed By: smas
    
    Differential Revision: http://phabricator.sabertoothventures.com/D62

--- Scripts/Scripts/Retrosheet/parseHistoricalBatterStats.php
+++ Scripts/Scripts/Retrosheet/parseHistoricalBatterStats.php
@@ -9,9 +9,11 @@ ini_set('mysqli.reconnect', '1');
 include('/Users/constants.php');
 include(HOME_PATH.'Scripts/Include/sweetfunctions.php');
 
-const MIN_PLATE_APPEARANCE = 0;
+const MIN_PLATE_APPEARANCE = 18;
+const NUM_DECIMALS = 3;
 
 function getRetrosheetQuery($season, $table) {
+    echo 'edit this dudes name back!!'."\n";
     $query =
         "SELECT
             player_name,
@@ -29,7 +31,9 @@ function getRetrosheetQuery($season, $table) {
             season,
             ds
         FROM $table
-        WHERE season = $season";
+        WHERE season = $season
+        AND ds = '1951-04-24'
+        AND player_id = 'rizzp101'";
     return $query;
 }
 
@@ -48,8 +52,15 @@ function updateBattingArray($batting_instance, $player_stats, $average_stats) {
     $ds = $batting_instance['ds'];
     $split = $batting_instance['split'];
     $plate_appearances = $batting_instance['plate_appearances'];
+    // If a player hasn't batted more than the min threshold don't fill
+    // his personal stats, but add him to the averages.
     if ($plate_appearances < MIN_PLATE_APPEARANCE) {
-        // TODO(cert): DO SOMETHING HERE
+        $average_stats[$ds][$split]['plate_appearances'] += $plate_appearances;
+        foreach ($batting_instance as $stat_name => $stat) {
+            if (in_array($stat_name, $pctStats)) {
+                $average_stats[$ds][$split][$stat_name] += $stat;
+            }
+        }
         return array($player_stats, $average_stats);
     }
     $player_stats[$player_id][$ds][$split]['plate_appearances'] =
@@ -59,12 +70,76 @@ function updateBattingArray($batting_instance, $player_stats, $average_stats) {
     foreach ($batting_instance as $stat_name => $stat) {
         if (in_array($stat_name, $pctStats)) {
             $stat_pct_name = array_search($stat_name, $pctStats);
-            $stat_pct = $stat / $plate_appearances;
+            $stat_pct = number_format($stat / $plate_appearances, NUM_DECIMALS);
             $player_stats[$player_id][$ds][$split][$stat_pct_name] = $stat_pct;
             $average_stats[$ds][$split][$stat_name] += $stat;
         }
     }
     return array($player_stats, $average_stats);
+}
+
+function updateMissingSplits(
+    $player_season,
+    $average_season,
+    $player_career = null
+) {
+    global $splits;
+    foreach ($player_season as $player_id => $dates) {
+        foreach ($dates as $date => $split_data) {
+            // Note: We are cycling through ALL splits (per the global var).
+            foreach ($splits as $split) {
+                // OPTION 1: Use Batter's Total Split.
+                // OPTION 2: Use Batter's Career Split.
+                // OPTION 3: User Batter' Career Total Split.
+                // OPTION 4: Use Average Stats For That Split.
+                if ($player_season[$player_id][$date][$split]) {
+                    continue;
+                } else {
+                    $player_season[$player_id][$date][$split] =
+                        $player_season[$player_id][$date]['Total'];
+                }
+                if (!$player_season[$player_id][$date][$split] &&
+                    $player_career[$player_id][$date]) {
+                    $player_season[$player_id][$date][$split] =
+                        $player_career[$player_id][$date][$split] ?
+                        $player_career[$player_id][$date][$split] :
+                        $player_career[$player_id][$date]['Total'];
+                }
+                if (!$player_season[$player_id][$date][$split]) {
+                    $player_season[$player_id][$date][$split] =
+                        $average_season[$date][$split];
+                }
+                $player_season[$player_id][$date][$split]
+                    ['plate_appearances'] = 0;
+            }
+        }
+    }
+    return $player_season;
+}
+
+function prepareMultiInsert($player_season) {
+    $player_insert = array();
+    foreach ($player_season as $player => $dates) {
+        foreach ($dates as $date => $splits) {
+            $player_insert[$player]['player_id'] = $player;
+            $player_insert[$player]['ds'] = $date;
+            $final_splits = array();
+            $defaults = 0;
+            $max_appearances = 0;
+            foreach ($splits as $split_name => $split) {
+                $split['player_id'] = $player;
+                $pas = $split['plate_appearances'];
+                $defaults += $pas ? 0 : 1;
+                $max_appearances =
+                    $pas > $max_appearances ? $pas : $max_appearances;
+                $final_splits[$split_name] = $split;
+            }
+            $player_insert[$player]['defaults'] = $defaults;
+            $player_insert[$player]['plate_appearances'] = $max_appearances;
+            $player_insert[$player]['stats'] = json_encode($final_splits);
+        }
+    }
+    return $player_insert;
 }
 
 function convertSeasonToPct($average_season) {
@@ -79,7 +154,7 @@ function convertSeasonToPct($average_season) {
                     if ($stat > 0) {
                         $pct_stat = $stat / $plate_appearances;
                         $average_pcts[$ds][$split_name][$stat_pct_name] =
-                            $pct_stat;
+                            number_format($pct_stat, NUM_DECIMALS);
                     } else {
                         $average_pcts[$ds][$split_name][$stat_pct_name] = 0;
                     }
@@ -91,7 +166,6 @@ function convertSeasonToPct($average_season) {
 }
 
 echo 'testing: change back to 1950'."\n";
-$test = 1;
 $season_table = "retrosheet_historical_batting";
 $career_table = "retrosheet_historical_batting_career";
 for ($season = 1951; $season < 2014; $season++) {
@@ -101,29 +175,32 @@ for ($season = 1951; $season < 2014; $season++) {
     $average_career = null;
     $season_data = getBattingData($season, $season_table);
     $career_data = getBattingData($season, $career_table);
-    foreach ($season_data as $index => $season_split) {
-        list($player_season, $average_season) = updateBattingArray(
-            $season_split,
-            $player_season,
-            $average_season
-        );
-        $career_split = $career_data[$index];
+    foreach ($career_data as $index => $career_split) {
         list($player_career, $average_career) = updateBattingArray(
             $career_split,
             $player_career,
             $average_career
         );
-        $test++;
-        if ($test == 3) {
-            print_r($player_season);
-            print_r($player_career);
-            break;
+        $season_split = $season_data[$index];
+        if ($season_split) {
+            list($player_season, $average_season) = updateBattingArray(
+                $season_split,
+                $player_season,
+                $average_season
+            );
         }
     }
+    // TODO(cert): Add bucket splits here (right now always defaulting).
     $average_season = convertSeasonToPct($average_season);
     $average_career = convertSeasonToPct($average_career);
-    print_r($average_season);
-    print_r($average_career);
+    // Go through the splits and fill in gaps with Totals or Averages.
+    $player_career = updateMissingSplits($player_career, $average_career);
+    $player_season =
+        updateMissingSplits($player_season, $average_season, $player_career);
+    $player_season = prepareMultiInsert($player_season);
+    $player_career = prepareMultiInsert($player_career);
+    print_r($player_season);
+    print_r($player_career);
     exit();
 /*
     multi_insert(
