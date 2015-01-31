831a7ec48b3cf5af10403df8e43aa29cc2fa564c
Sarah Masimore <sarahsmasimore@gmail.com>
2015-01-31 14:15:56 -0800

    [Sim] First set of simulation runs
    Summary: Title
    
    Test Plan: python run.py
    
    Reviewers: cert
    
    Reviewed By: cert
    
    Differential Revision: http://phabricator.sabertoothventures.com/D146

--- Scripts/Simulation/Sim2/run.py
+++ Scripts/Simulation/Sim2/run.py
@@ -2,18 +2,18 @@ from Simulation import Simulation
 from Game import StatCategories
 
 weights = {
-    StatCategories.B_TOTAL : .5,
-    StatCategories.P_TOTAL : .5,
+    StatCategories.B_TOTAL : 1.0,
 }
 
 season_start = 1955
-season_end = 1955
-stats_year = 'career'
+season_end = 2013
+stats_years = ['career', 'previous', 'season']
 stats_type = 'basic'
 
 for season in range(season_start, season_end + 1):
-    print 'Running ' + str(season) + '...'
-    test = Simulation(weights, season, stats_year, stats_type)
-    test.setUseReliever(False)
-    test.setSample(0,9)
-    test.run()
+    for stats_year in stats_years:
+        print 'Running ' + str(season) + ' ' + stats_year + '...'
+        test = Simulation(weights, season, stats_year, stats_type)
+        test.setUseReliever(False)
+        test.setSample(0,9)
+        test.run()
