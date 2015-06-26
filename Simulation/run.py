8721b09a5ec53c9ff20fd7247fa78b0429cad7dd
baseball <sarahsmasimore@gmail.com>
2015-06-26 13:13:04 -0700

    [sim] today's date in run.py
    Summary: Title
    
    Test Plan: python run.py
    
    Reviewers: cert
    
    Reviewed By: cert
    
    Differential Revision: http://phabricator.sabertoothventures.com/D252

--- Simulation/run.py
+++ Simulation/run.py
@@ -1,20 +1,20 @@
 from Simulation import Simulation
 from Game import StatCategories
+from datetime import date
 
 weights = {
-    StatCategories.B_TOTAL : .5,
-    StatCategories.B_HOME_AWAY : .5,
+    StatCategories.B_HOME_AWAY : 1.0,
 }
 
-season_start = 2007
-season_end = 2008
-stats_years = ['career', 'previous', 'season']
+season_start = 2015
+season_end = 2015
+stats_years = ['career']
 stats_type = 'basic'
 
 for season in range(season_start, season_end + 1):
     for stats_year in stats_years:
         print 'Running ' + str(season) + ' ' + stats_year + '...'
         test = Simulation(weights, season, stats_year, stats_type)
+        test.setGameDate(str(date.today()))
         test.setUseReliever(False)
-        test.setSample(0,9)
         test.run()
